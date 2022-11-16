c6425a77917acf1503a177c8bb4252e6e21c35e6
AviralTanwar <aviraltanwar@gmail.com>
2022-11-17 00:27:21 +0530

    App icon not done ig

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,196 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomeRoute(),
+      '/second': (context) => const SecondRoute(),
+      // '/third': (context) => const ThirdRoute(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/bg_image.jpg'),
+                  opacity: 0.5,
+                  fit: BoxFit.cover)),
+          child: Scaffold(
+              backgroundColor: Colors.white38,
+              appBar: AppBar(
+                  elevation: 0,
+                  backgroundColor: Colors.white60,
+                  leading: Image.asset('images/jiit_logo.png'),
+                  title: Center(
+                      child: Text('Welcome to JIIT',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Colors.black)))),
+              body: Container(
+                  child: SafeArea(
+                      child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                    child: Text('Admin Login Page',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black)),
+                  ),
+                  Container(
+                      padding: const EdgeInsets.fromLTRB(40, 30, 40, 30),
+                      child: TextField(
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.black),
+                        // controller: nameController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white70,
+                          border: InputBorder.none,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.white70),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                          ),
+                          labelText: 'User Name',
+                          // errorText: 'Wrong User Name',
+                        ),
+                      )),
+                  Container(
+                      // padding: EdgeInsets.fromLTRB(250, 50, 60, 10),
+                      padding: const EdgeInsets.fromLTRB(40, 0, 40, 10),
+                      child: TextField(
+                          obscureText: true,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.black),
+                          // controller: nameController,
+                          decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white70,
+                            border: InputBorder.none,
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.white70),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20)),
+                            ),
+                            labelText: 'Password',
+                            // errorText: 'Wrong Password',
+                          ))),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/second');
+                      },
+                      child: Center(
+                          widthFactor: 1,
+                          heightFactor: 1,
+                          child: Text(
+                            'Sign In',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          )))
+                ],
+              ))))),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SecondRoute extends StatefulWidget {
+  const SecondRoute({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SecondRoute> createState() => _SecondRouteState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SecondRouteState extends State<SecondRoute> {
+  String _scanBarcode = 'Unknown';
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      // print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _scanBarcode = barcodeScanRes;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    // TODO: implement build
+    return MaterialApp(
+        home: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/bg_image.jpg'),
+              opacity: 0.5,
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.white38,
+        appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white60,
+            leading: Image.asset('images/jiit_logo.png'),
+            title: Center(
+                child: Text('Welcome to JIIT',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.black)))),
+        body: SafeArea(
+            child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 60, horizontal: 40),
+              child: Text(
+                'Scan the QR code using this scanner to start the admission process',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 18),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 60),
+              child: ElevatedButton(
+                  onPressed: () => scanQR(),
+                  child: const Text(
+                    'QR scan',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 13),
+                  )),
+            )
           ],
-        ),
+        )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
